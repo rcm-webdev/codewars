100876598425b346fba4933dc22b96fa87276106
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-07-19 18:18:42 -0700

    add code wars challenge from 07-19-23

---
+++ 8kyu/you-only-need-one.js
@@ -0,0 +1,26 @@
+
+
+//Name: You only need one
+//Difficulty: 8kyu
+//Tags: Fundamentals, Arrays, Strings
+
+//-------------------------------------------------------------------------------------------------------------------------
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution 
+function check(a, x) {
+    // your code here
+    return a.includes(x);
+  }
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+const check = (a,x) => a.includes(x);
