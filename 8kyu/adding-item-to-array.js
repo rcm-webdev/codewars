c3fb62b3b28ca96f14765bea4c3be4c16fa73209
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-08-18 17:02:16 -0700

    added codewars challenge from 08-18-23

---
+++ 8kyu/adding-item-to-array.js
@@ -0,0 +1,25 @@
+//Name: Basic Training: Add item to an Array
+//Difficulty: 8kyu
+//Tags: Fundamentals, Arrays
+
+//-------------------------------------------------------------------------------------------------------------------------
+// Description
+
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution
+
+website.push("codewars");
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+
+//same as above
