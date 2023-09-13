097f1d81fe275a78a911e74c37447266d0414ac6
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-09-13 01:17:10 -0700

    added codewars challenge from 09-13-23

---
+++ 8kyu/welcome.js
@@ -0,0 +1,110 @@
+//Name: Welcome
+//Difficulty: 8kyu
+//Tags: Fundamentals
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+// Description
+
+// Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+// The Task
+
+//     Think of a way to store the languages as a database. The languages are listed below so you can copy and paste!
+//     Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+
+// The Database
+
+// [ ("english", "Welcome")
+// , ("czech", "Vitejte")
+// , ("danish", "Velkomst")
+// , ("dutch", "Welkom")
+// , ("estonian", "Tere tulemast")
+// , ("finnish", "Tervetuloa")
+// , ("flemish", "Welgekomen")
+// , ("french", "Bienvenue")
+// , ("german", "Willkommen")
+// , ("irish", "Failte")
+// , ("italian", "Benvenuto")
+// , ("latvian", "Gaidits")
+// , ("lithuanian", "Laukiamas")
+// , ("polish", "Witamy")
+// , ("spanish", "Bienvenido")
+// , ("swedish", "Valkommen")
+// , ("welsh", "Croeso")
+// ]
+
+// Possible invalid inputs include:
+
+// IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+// IP_ADDRESS_NOT_FOUND - ip address not in the database
+// IP_ADDRESS_REQUIRED - no ip address was supplied
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution
+
+function greet(language) {
+    // Define the database as an object
+    const languageDatabase = {
+      english: "Welcome",
+      czech: "Vitejte",
+      danish: "Velkomst",
+      dutch: "Welkom",
+      estonian: "Tere tulemast",
+      finnish: "Tervetuloa",
+      flemish: "Welgekomen",
+      french: "Bienvenue",
+      german: "Willkommen",
+      irish: "Failte",
+      italian: "Benvenuto",
+      latvian: "Gaidits",
+      lithuanian: "Laukiamas",
+      polish: "Witamy",
+      spanish: "Bienvenido",
+      swedish: "Valkommen",
+      welsh: "Croeso",
+    };
+  
+    // Check if the language exists in the database
+    if (languageDatabase.hasOwnProperty(language.toLowerCase())) {
+      return languageDatabase[language.toLowerCase()];
+    } else {
+      // Default to English if the language is not found or is invalid
+      return languageDatabase.english;
+    }
+  }
+  
+  // Example usages:
+  console.log(greet("german")); // Output: "Willkommen"
+  console.log(greet("russian")); // Output: "Welcome" (default to English)
+  console.log(greet("IP_ADDRESS_NOT_FOUND")); // Output: "Welcome" (default to English)
+  
+
+//Best Practices
+
+function greet(lang) {
+	return langs[lang]||langs['english'];
+}
+
+var langs = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+};
