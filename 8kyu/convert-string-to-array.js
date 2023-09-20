2e3fa6e0f0a72360b26db8a37482cf27cf47fb8a
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-09-19 18:02:55 -0700

    added codewars challenge from 9-19-23

---
+++ 8kyu/convert-string-to-array.js
@@ -0,0 +1,32 @@
+//Name: Convert a string to an array
+//Difficulty: 8kyu
+//Tags: Fundamentals, Arrays, Strings
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+// Description
+
+// Write a function to split a string and convert it into an array of words.
+// Examples (Input ==> Output):
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution
+
+function stringToArray(string){
+
+	return string.split(' ')
+
+}
+  
+
+//Best Practices
+  
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+  }
