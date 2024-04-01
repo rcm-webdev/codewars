204b3bee45b36bb8bbbf2dcd04017f631c4941a1
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-04-01 08:46:52 -0700

    update code wars challenge //04/01/24

--- 7kyu/reversewords.js
+++ 7kyu/reversewords.js
@@ -36,3 +36,11 @@ function reverseWords(str) {
 //-------------------------------------------------------------------------------------------------------------------------
 
 //Best Practices
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map(function (word) {
+      return word.split("").reverse().join("");
+    })
+    .join(" ");
+}
