5ce497e5e7ebeef1da525cf3d39b478865f2363f
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-04-03 10:50:34 -0700

    codewars challenge 04-03-24

---
+++ 8kyu/remove-exclamationmarks.js
@@ -0,0 +1,32 @@
+//Name: Exclamation Makrks
+//Difficulty: 8kyu
+//Tags: Fundamentals, Strings
+
+//-------------------------------------------------------------------------------------------------------------------------
+//Description
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Code + Pseudo Code
+
+function removeExclamationMarks(s) {
+  //spread operator to convert string into an array
+  //then filter out '!'
+  //join method
+  return [...s].filter((value) => value !== "!").join("");
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+function reverseWords(str) {
+  c;
+  return str
+    .split(" ")
+    .map(function (word) {
+      return word.split("").reverse().join("");
+    })
+    .join(" ");
+}
