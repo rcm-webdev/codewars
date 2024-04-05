65eac1d99db44d29aca213597bfda9a4a5dc3bb3
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-04-05 09:17:15 -0700

    codewars challenge from 04/05/24

---
+++ 8kyu/convertToString.js
@@ -0,0 +1,31 @@
+//Name: Convert Number to String
+//Difficulty: 8kyu
+//Tags: Fundamentals, Strings
+
+//-------------------------------------------------------------------------------------------------------------------------
+//Description
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Code + Pseudo Code
+
+//use toString method to convert number (integer) into a string
+
+function numberToString(num) {
+  return num.toString();
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+
+//  same as mentioned above
