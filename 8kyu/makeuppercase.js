7618262c18aa27a76301eaba70c1f979011fd79c
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-05-03 00:45:07 -0700

    complete codewars challenge from 05/03/24

---
+++ 8kyu/makeuppercase.js
@@ -0,0 +1,51 @@
+//Name: MakeUpperCase
+//Difficulty: 8kyu
+//Tags: Fundamentals, strings
+
+//-------------------------------------------------------------------------------------------------------------------------
+//Description
+
+// Write a function which converts the input string to uppercase.
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution w/ PREP
+
+//parameters: string values
+//results: make each letter an uppercase letter
+//example: makeUpperCase('goodbye') //GOODBYE
+//pseudocode:
+
+//use toUpperCase method
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+//OR
+
+//not to use a method
+
+function makeUpperCase(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    // Check if the character is a lowercase letter
+    if (char >= "a" && char <= "z") {
+      // Convert to uppercase by subtracting 32 from the ASCII value
+      result += String.fromCharCode(char.charCodeAt(0) - 32);
+    } else {
+      // If it's not a lowercase letter, just append the character as is
+      result += char;
+    }
+  }
+
+  return result;
+}
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
