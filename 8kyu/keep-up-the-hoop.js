545d09b9eece15c20e8f745e7b75c26701030240
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-05-09 12:30:41 -0700

    complete codewars challenge from 05/09/24

---
+++ 8kyu/keep-up-the-hoop.js
@@ -0,0 +1,37 @@
+//Name: Keep up the hoop
+//Difficulty: 8kyu
+//Tags: Fundamentals
+
+//-------------------------------------------------------------------------------------------------------------------------
+//Description
+
+// You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+// Note that for some languages, the function main is the entry point of the program.
+
+// Here's how it will be tested:
+
+//     Solution.main("parameter1","parameter2");
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution w/ PREP
+
+//parameters: no parameters
+//results: main method should print 'Hello World!'
+//example: Solution.main() //'Hello World'
+//pseudocode
+
+class Solution {
+  //define static method
+  static main() {
+    //print 'Hello World' to console
+    console.log("Hello World!");
+  }
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+
+//same as above
