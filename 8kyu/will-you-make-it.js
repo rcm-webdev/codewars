25a639182d3980bb1a0125d623a90aacc58f3eb4
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-05-10 09:23:47 -0700

    complete codewars challenge from 05/10/24

---
+++ 8kyu/will-you-make-it.js
@@ -0,0 +1,31 @@
+//Name: Will you make it
+//Difficulty: 8kyu
+//Tags: Fundamentals, Mathematics
+
+//-------------------------------------------------------------------------------------------------------------------------
+//Description
+
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution w/ PREP
+
+//parameters: numbers
+//results: boolean (can you or can you not make it to a gas station)
+//example zeroFuel = (100, 25, 1) //false
+//pseudocode
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return mpg * fuelLeft >= distanceToPump ? true : false;
+};
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+
+//same as above
