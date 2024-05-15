d1330704ed47e1256e616342d1e4510d34d96ae0
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-05-15 10:41:41 -0700

    complete codewars challenge from 05/15/24

---
+++ 8kyu/combat-function.js
@@ -0,0 +1,30 @@
+//Name: Combat Function
+//Difficulty: 8kyu
+//Tags: Fundamentals
+
+//-------------------------------------------------------------------------------------------------------------------------
+//Description
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution w/ PREP
+
+//parameters: numbers, each input is valid and will always be a number (no strings)
+//results: input health and then current damage (which i am assuming that it is subtracted from health), health can never be below 0
+//example: function combat(100, 50) // 50
+//pseudocode
+
+function combat(health, damage) {
+  let currentHealth = health - damage;
+  return currentHealth >= 0 ? currentHealth : 0;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+
+function combat(health, damage) {
+  return health < damage ? 0 : health - damage;
+}
