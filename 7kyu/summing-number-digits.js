2ce941508897b896b1e447a09c786dc32a18c379
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-05-28 10:31:49 -0700

    update codewars challenge from 5/16/24 with a function without any methods

--- 7kyu/summing-number-digits.js
+++ 7kyu/summing-number-digits.js
@@ -34,6 +34,36 @@ function sumDigits(number) {
   return numToString;
 }
 
+//now lets work through this problem without any methods
+
+//
+function sumDigits2(number) {
+  //initialize sum variable
+  let sum = 0;
+
+  //handle any negative values
+  if (number < 0) {
+    number = -number;
+  }
+
+  //convert number to a string
+  let numString = "";
+  let n = number;
+  while (n > 0) {
+    let digit = n % 10;
+    numstring = String.fromCharCode(digit + "0".charCodeAt(0)) + numString;
+    n = (n - digit) / 10;
+  }
+
+  //iterate through string of number
+
+  for (let i = 0; i < numString.length; i++) {
+    sum += numString.charCodeAt(i) - "0".charCodeAt(0);
+  }
+
+  return sum;
+}
+
 //-------------------------------------------------------------------------------------------------------------------------
 
 //Best Practices
