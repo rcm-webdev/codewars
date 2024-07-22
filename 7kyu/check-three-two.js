fdc95c2a10d5a5ec1a97f0e767a079ad2da1e4c3
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-07-21 20:50:15 -0700

    complete codewars challenge from 07/21/24

---
+++ 7kyu/check-three-two.js
@@ -0,0 +1,56 @@
+//Name: Check three and two
+//Difficulty: 7kyu
+//Tags: Fundamentals, Arrays
+
+//-------------------------------------------------------------------------------------------------------------------------
+//Description
+
+// Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+// Examples
+
+// ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+// ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+// ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//My Solution w/ PREP
+
+//parameters: all valid inputs, all inputs are strings
+//results: boolean if there is more than 2 or 3, no more, no less
+//example: checkThreeAndTwo(['a','b', 'b', 'a', 'a'])// true, 3x a and 2x b
+//pseudocode:
+
+function checkThreeAndTwo(array) {
+  //lets initialize a counter for each letter
+  let counterA = 0,
+    counterB = 0,
+    counterC = 0;
+
+  //count occurences
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "a") {
+      counterA++;
+    } else if (array[i] === "b") {
+      counterB++;
+    } else {
+      counterC++;
+    }
+  }
+
+  // Check if there are exactly three of one value and exactly two of another
+  return (
+    (counterA === 3 && counterB === 2) ||
+    (counterA === 3 && counterC === 2) ||
+    (counterB === 3 && counterA === 2) ||
+    (counterB === 3 && counterC === 2) ||
+    (counterC === 3 && counterA === 2) ||
+    (counterC === 3 && counterB === 2)
+  );
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//Best Practices
+
+//
