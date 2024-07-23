e38620173f1e5715b45768ca2eb316acf6a1a781
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-07-22 20:54:27 -0700

    update codewars challenge from 07/21/24

--- 7kyu/check-three-two.js
+++ 7kyu/check-three-two.js
@@ -75,4 +75,19 @@ function checkThreeAndTwo(array) {
 
 //Best Practices
 
-//
+function checkThreeAndTwo(array) {
+  const counts = array.reduce((result, letter) => {
+    if (result[letter]) {
+      result[letter]++;
+    } else {
+      result[letter] = 1;
+    }
+
+    return result;
+  }, {});
+
+  const containsThree = Object.values(counts).some((x) => x === 3);
+  const containsTwo = Object.values(counts).some((x) => x === 2);
+
+  return containsThree && containsTwo;
+}
